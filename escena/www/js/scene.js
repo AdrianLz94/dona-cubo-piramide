1d4df423a4ff8a72a59049abb2bb1d399bb429da
AdrianLz94 <31250029+AdrianLz94@users.noreply.github.com>
2017-11-13 17:11:53 -0700

    Escena Final

--- escena/www/js/scene.js
+++ escena/www/js/scene.js
@@ -1,22 +1,19 @@
-(function(){
-
-    //vertex shader calcular posiciones y vertices de los primitivos
-    //y el frament shader calcula el color y la posicion de los primitivos
-
-    let scene = new THREE.Scene();
-    const aspectRatio = window.innerWidth / window.innerHeight;
-    let camera = new THREE.PerspectiveCamera(110, aspectRatio, 0.1, 150);
-    let renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-    renderer.shadowMap.enabled = true;
-    renderer.shadowMap.soft = true;
-    renderer.shadowMap.type = THREE.PCFShadowMap;
+(function(){let scene = new THREE.Scene();
+ const aspectRatio = window.innerWidth / window.innerHeight;
+ let camera = new THREE.PerspectiveCamera(110, aspectRatio, 0.1, 150);
+ let renderer = new THREE.WebGLRenderer();
+ renderer.setSize(window.innerWidth, window.innerHeight);
+ document.body.appendChild(renderer.domElement);
+ renderer.shadowMap.enabled = true;
+ renderer.shadowMap.soft = true;
+ renderer.shadowMap.type = THREE.PCFShadowMap;
 
 
     camera.position.z = 10;
     camera.position.y = 90;
     camera.rotation.x = -1.5;
+
+    alert('Con la Tecla "D" cambia la DONA, con la "P" la PIRAMIDE, con la "C" el CUBO y con la tecla BackSpace regresa al original');
 
     let planeGeometry = new THREE.PlaneGeometry(200,900);
     planeGeometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI/2));
@@ -28,12 +25,8 @@
     let mesh;
     let dona;
    let piramide;
-
-    let loader = new THREE.TextureLoader();
-  //  let loader2 = new THREE.TextureLoader();
   
-
-    
+    let loader = new THREE.TextureLoader();
 
     loader.load('public/mapa2.jpg', function(texture){
 
@@ -59,11 +52,14 @@
         scene.add(mesh1);
 */
     });
+   
    loader.load('public/chocolate.jpg', function(texture){
     let geometry1 = new THREE.TorusGeometry( 15, 5, 20, 100 );
-    let material = new THREE.MeshBasicMaterial( { 
-       // color: 0x7A2E11 
+    let material = new THREE.MeshBasicMaterial( {
+       
+ //color: 0x7A2E11 
      map: texture
+     
      } )
      dona = new THREE.Mesh( geometry1, material );
     dona.castShadow = true;
@@ -107,8 +103,51 @@ loader.load('public/parena.jpg', function(texture){
     scene.add(new THREE.AmbientLight(0x404040));
     scene.add(plane);
     scene.add(pointLight);
+    ////////////////////
+   // handleKeys();
+   
+   document.body.onkeyup=function(tecla){  // Dectecta si se presiona alguna tecla
   
-    
+    if (tecla.keyCode == 80) { 
+        texturep = new THREE.TextureLoader().load('public/agua.jpg');
+    }
+    if (tecla.keyCode == 68) { 
+        textured = new THREE.TextureLoader().load('public/ob.jpg');
+    }
+    if (tecla.keyCode == 67) { 
+        texturec = new THREE.TextureLoader().load('public/mapa1.jpg');
+    }
+    if (tecla.keyCode == 8) { 
+        textured = new THREE.TextureLoader().load('public/chocolate.jpg');
+        texturep = new THREE.TextureLoader().load('public/parena.jpg');
+        texturec = new THREE.TextureLoader().load('public/mapa2.jpg');
+    }
+  
+  
+       dona.material = new THREE.MeshBasicMaterial({map:textured})
+       piramide.material = new THREE.MeshBasicMaterial({map:texturep})
+       mesh.material = new THREE.MeshBasicMaterial({map:texturec})
+       renderer.render(scene, camera);
+                                 
+   }
+                                           //Controles de orbita, sirve para mover la camara
+                                           
+                 /*                          
+var currentlyPressedKeys = {};
+   
+function handleKeyDown(event) {
+currentlyPressedKeys[event.keyCode] = true;
+ if(String.fromCharCode(event.keyCode) == "F"){                
+              texture = new THREE.TextureLoader().load('public/metal.jpg');
+             }
+          else{
+              texture = new THREE.TextureLoader().load('public/mapa1.jpg');
+             }}
+                                           
+         function handleKeyUp(event) {
+           currentlyPressedKeys[event.keyCode] = false;
+           }
+              */                              
 
     let controls = new THREE.OrbitControls(camera, renderer.domElement);
 
